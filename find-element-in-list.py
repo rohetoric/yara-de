9adf8a0f63d2d7fb9c302147996a35e8352445b3
rohetoric <rohitbhardwaj434@gmail.com>
2023-08-13 14:57:57 +0530

    Adding solution - finding first and last position of element in list

---
+++ find-element-in-list.py
@@ -0,0 +1,61 @@
+def find_target_in_nums(nums:list,target:int) -> list[int,int]:
+    first_position,last_position=-1,-1
+
+    if nums == []:
+        return [first_position,last_position]
+    
+    start_seek = 0
+    stop_seek = len(nums) - 1
+
+    #finding the first position
+    while start_seek <= stop_seek:
+        midpoint = start_seek + (stop_seek-start_seek)//2
+        if nums[midpoint] == target:
+            first_position = midpoint
+            stop_seek = midpoint - 1
+        elif nums[midpoint] < target:
+            start_seek = midpoint + 1
+        else:
+            stop_seek = midpoint - 1
+
+    start_seek = 0
+    stop_seek = len(nums) - 1
+
+    #finding the last position
+    while start_seek <= stop_seek:
+        midpoint = start_seek + (stop_seek-start_seek)//2
+        if nums[midpoint] == target:
+            last_position = midpoint
+            start_seek = midpoint + 1
+        elif nums[midpoint] < target:
+            start_seek = midpoint + 1
+        else:
+            stop_seek = midpoint - 1
+
+    return [first_position,last_position]
+    
+
+
+if __name__ == "__main__":
+    
+    # creating an empty list
+    nums = []
+    
+    # number of elements as input
+    n = int(input("Enter number of list elements : ")) # Example - 6
+
+    print("Enter elements into the list. Press Enter before entering the elements after the first element!")
+    for i in range(0, n):
+        ele = int(input())
+        nums.append(ele) 
+    
+    print(f"Entered list is :: {nums}")
+
+    target = int(input("Enter the target: "))
+
+    position = find_target_in_nums(nums=nums,target=target)
+
+    print(position)
+
+
+
